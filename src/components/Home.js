b24eb695edcb3c0b2038380693c296d286c45996
iiy4383 <znlspf123@naver.com>
2021-05-30 22:10:13 +0900

    2021053001 by leejong

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,23 @@
 const Home = () => {
-  return <div>Hi!</div>;
+  return (
+    <div class="main-home-wrapper">
+      <div class="main-home-header">
+        <pre>
+          <h2>안녕하십니까!</h2>
+          <h3>한국교통대학교 소프트웨어학과</h3>
+          <h3>웹 어플리케이션 동아리</h3>
+          <h2>f(x)입니다.</h2>
+        </pre>
+      </div>
+      <div class="main-home-content">
+        <pre>
+          <h2>하루를 잘 보내고 계신가요?</h2>
+          <h3>날씨가 많이 더워지고 있습니다.</h3>
+          <h4>감기 조심하세요.</h4>
+        </pre>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,24 @@
 const Profile = () => {
-  return <div>Hello!</div>;
+  return (
+    <div class="main-profile-wrapper">
+      <div class="main-profile-header">
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+            <td>4</td>
+            <td>5</td>
+          </tr>
+        </tbody>
+      </div>
+      <div class="main-profile-content">
+        <thead class="main-profile-content-thead">1</thead>
+        <tbody class="main-profile-content-tbody">2</tbody>
+        <tfoot class="main-profile-content-tfoot">3</tfoot>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
