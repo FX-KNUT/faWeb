b8848acd57a54165a82491d41f211f8955413c78
iiy4383 <znlspf123@naver.com>
2021-06-08 20:27:13 +0900

    20210608 location and community a bit

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,15 +1,44 @@
 import { store } from './App.tsx';
 
-const Location = () => {
-  store.dispatch({ type: { loc: 'location' } });
+const Community = () => {
+  store.dispatch({ type: { loc: 'community' } });
 
   return (
-    <div className="main-location-wrapper">
-      <div className="main-location-content">
-        <p>장소입니다.</p>
+    <div className="main-community-wrapper">
+      <div className="main-community-content">
+        <span className="main-community-content-span">전체 게시글: 0 / 0</span>
+        <div className="main-community-posts">
+          <span className="main-community-posts-no">번호</span>
+          <span className="main-community-posts-title">제목</span>
+          <span className="main-community-posts-author">글쓴이</span>
+          <span id="1" className="main-community-posts-no-example">
+            1
+          </span>
+          <span id="some_title" className="main-community-posts-title-example">
+            한국교통대학교 소프트웨어학과
+          </span>
+          <span id="leejong" className="main-community-posts-author-example">
+            신이종
+          </span>
+        </div>
+        <div className="main-community-functions">
+          <button>등록</button>
+          <button>수정</button>
+          <button>삭제</button>
+        </div>
+      </div>
+      <div className="main-community-pagination">
+        <button className="main-community-pagination-start">처음으로</button>
+        <ul className="main-community-pagination-ul">
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+        </ul>
+        <button className="main-community-pagination-end">끝으로</button>
       </div>
     </div>
   );
 };
 
-export default Location;
+export default Community;
