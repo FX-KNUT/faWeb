38bc6ec1edf7679dfae640e37513d0fd85d7b3bb
iiy4383 <znlspf123@naver.com>
2021-06-14 19:31:46 +0900

    Axios package added, login-submitBtn logic annotated, Type typing in Login.tsx

---
+++ src/components/Constants.ts
@@ -0,0 +1,2 @@
+export const SERVERURL: string = 'SERVERURL LIKE 192.168.0.1';
+export const SERVERPORT: string = 'SERVERPORT LIKE :8080';
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,75 +1,96 @@
 import { useState } from 'react';
+// import { SERVERURL, SERVERPORT } from './Constants';
+// import axios from 'axios';
+
+// 백엔드와 교환할 데이터에 대한 정보를 인터페이스에 즉각 메모하세요.
+// 이것 또한 협업과정에서 타입스크립트가 가져다 주는 큰 장점들 중 하나입니다.
+// interface User { // 받는 유저 정보의 스키마
+
+// }
+
+// interface UserDTO { // 보내는 유저 정보의 스키마
+
+// }
 
 const Login = (): JSX.Element => {
 
-    let [id, setId] = useState('');
-    let [pw, setPw] = useState('');
+    // destructuring에 따른 Typing 방법
+    let [id, setId]: [ id: string, setId: Function] = useState('');
+    let [pw, setPw]: [ pw: string, setPw: Function] = useState('');
 
-    const modal = document.querySelector("#modal");
-    const login = document.querySelector("#login");
+    // Union Type, | 를 사용하여 이 타입 중 하나일 것임을 명시
+    // HTMLElement의 경우 로딩 시점에 따라 null로 인식될 수 있으므로 null까지 포함해야 함
+    const modal: HTMLElement | null = document.querySelector("#modal");
+    const login: HTMLElement | null = document.querySelector("#login");
 
-    const onIDChange = (e: any): void => {
+    // 굳이 Inline Style을 지정한다면 그나마 좀 더 괜찮게 하는 방법입니다.
+    const btnStyle = {  // 이런 객체엔 Typing하지 않아도 됩니다.
+        color: "white",
+        background: "purple",
+        padding: ".375rem .75rem",
+        border: "1px solid purple",
+        borderRadius: ".25rem",
+        fontSize: "1rem",
+        lineHeight: 1.5
+    };
+
+    // 객체의 타입을 다 외울 필요 없습니다.
+    // 아래의 onIDChange를 예로 들면, onIDChange 이벤트가 걸려있는
+    // DOM에 마우스를 올려 해당 객체의 타입이 무엇인지 확인하면 쉽게 알 수 있습니다.
+
+    const onIDChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
         setId(e.target.value)
     };
 
-    const onPasswordChange = (e: any): void => {
+    const onPasswordChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
         setPw(e.target.value)
     };
 
-    const onXClicked = (e: any): void => {
+    const onXClicked = (e: React.MouseEvent<HTMLButtonElement>): void => {
         e.preventDefault();
         modal?.classList.toggle("show");
         login?.classList.toggle("show");
     };
 
+    // axios를 이렇게 사용하는 방법도 좋지만,
+    // axios를 좀 더 날렵하게 custom하는 방법도 요긴하게 사용될 수 있습니다.
+    // 소규모 프로젝트에선 상관 없지만, 큰 프로젝트에선 DRY한 코드를 위해
+    // axios를 Hook처럼 커스텀하여 사용할 수 있습니다.
+    // const requsetUserInfo = async (UserDTO: UserDTO) => {
+    //     // axios를 하단 코드처럼 사용하지 않고
+    //     // axios({ method: 'post', ... }) 처럼 사용해도 좋습니다.
+    //     const res: User = 
+    //         await axios.get(`${SERVERURL}:${SERVERPORT}`, UserDTO);
+    //     return res;
+    // }
+
+    // 패키지에 axios 설치 되어 있습니다. axios 사용하세요.
+    // const onSubmit = (e: React.ChangeEvent<HTMLFormElement>): void => {
+    //     e.preventDefault();
+    //     const UserDTO: UserDTO = {
+    //         id,
+    //         pw
+    //     }
+    //     const res = requsetUserInfo(UserDTO);
+    //     console.log(res);
+    // }
+
+    const onSubmit = (e: React.ChangeEvent<HTMLFormElement>): void => {
+        e.preventDefault();
+    }
+
     return (
         <div id="login-wrapper">
-            <form action="" method="post">
-                <table>
-                    <thead>
-                        <tr>
-                            <td>
-                                <p id="login-thead-login_p">로그인</p>
-                            </td>
-                            <td>
-                                <button onClick={onXClicked} id="login-X_Btn">X</button>
-                            </td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>
-                                <p>ID</p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <input type="text" value={id} onChange={onIDChange} />    
-                            </td>                          
-                        </tr>
-                        <tr>
-                            <td>
-                                <p>
-                                    Password
-                                </p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <input type="password" name="" id="" value={pw} onChange={onPasswordChange} />
-                            </td>
-                        </tr>
-                    </tbody>
-                    <tfoot>
-                        <tr>
-                            <td><button type="submit">Sign In</button></td>
-                        </tr>
-                    </tfoot>
-                </table>             
+            <form action="" method="post" onSubmit={onSubmit}>
+                <p id="login-header" className="login-p">로그인</p>
+                <button onClick={onXClicked} id="login-X_Btn">X</button>
+                <p id="login-id" className="login-p">ID</p>
+                <input type="text" value={id} className="login-input" onChange={onIDChange} />    
+                <p id="login-pw" className="login-p">Password</p>
+                <input type="password" name="" id="" className="login-input" value={pw} onChange={onPasswordChange} />
+                <button id="login-submitBtn" style={btnStyle} type="submit">Sign In</button>
             </form>
-  
         </div>
-
     );
 }
 
